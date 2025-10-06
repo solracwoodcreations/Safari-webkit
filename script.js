bd044e6114641dc8e12af58341778b8b8a5cc5e9
solrac <119016954+solracwoodcreations@users.noreply.github.com>
2025-10-06 08:58:39 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
 // script.js
 if ('paintWorklet' in CSS) {
-  CSS.paintWorklet.addModule('paint.js');
+  const blob = new Blob([`
+    class StripesPainter {
+      static get inputProperties() { return []; }
+
+      paint(ctx, size) {
+        const { width, height } = size;
+        const stripeHeight = 10;
+
+        for (let y = 0; y < height; y += stripeHeight * 2) {
+          ctx.fillStyle = "#ff4081";
+          ctx.fillRect(0, y, width, stripeHeight);
+        }
+      }
+    }
+
+    registerPaint("stripes", StripesPainter);
+  `], { type: 'application/javascript' });
+
+  const blobURL = URL.createObjectURL(blob);
+  CSS.paintWorklet.addModule(blobURL);
 } else {
-  console.warn('Paint API not supported');
+  console.warn('CSS.paintWorklet not supported in this browser.');
 }
