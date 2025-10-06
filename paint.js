6a23ae4016cf4236c837bc3677554e673acba496
solrac <119016954+solracwoodcreations@users.noreply.github.com>
2025-10-06 10:14:27 -0400

    Update paint.js

--- paint.js
+++ paint.js
@@ -1,18 +1,1 @@
-// paint.js
-class StripesPainter {
-  static get inputProperties() {
-    return [];
-  }
-
-  paint(ctx, size) {
-    const { width, height } = size;
-    const stripeHeight = 10;
-
-    for (let y = 0; y < height; y += stripeHeight * 2) {
-      ctx.fillStyle = "#ff4081";
-      ctx.fillRect(0, y, width, stripeHeight);
-    }
-  }
-}
-
-registerPaint("stripes", StripesPainter);
+ 
